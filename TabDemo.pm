66827d5ddf72eecd5cd8523498d8ace8c5529522
skyter10086 <skyter10086@aliyun.com>
2019-10-08 01:06:10 +0800

    Update TabDemo.pm
    asdasdasd

--- TabDemo.pm
+++ TabDemo.pm
@@ -1,13 +1,27 @@
 package TabDemo;
-use DBO;
-use Schema;
+use DBObject;
+use Schema; 
 
 use Moo;
 use namespace::autoclean;
 
+my $db = DBObject->new(
+        source=>"dbi:SQLite:dbname=test.db",
+        user=>"",
+        auth=>"",
+        options=>{sqlite_unicode => 1,RaiseError => 1, AutoCommit => 1},);
+my $schema = Schema->new(
+        name => 'company',
+        fields => [
+            Field->new(name=>'name',type=>'VARCHAR',len=>60,primary=>1),
+            Field->new(name=>'property',type=>'NUM',len=>12,default=>0.0,not_null=>1),
+            Field->new(name=>'people',type=>'NUM',len=>10,not_null=>1),
+        ],);
+        
 has db => (
     is => 'ro',
     handles => 'DBO',
+    default => sub { $db},
 );
 
 has scm => (
@@ -15,6 +29,7 @@ has scm => (
     does => 'Schem',
     handles => {schema => 'build',
 		        primary => 'pk'},
+    default => sub {$schema},
     );
 
 
